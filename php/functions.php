8bdf8baf95e6d2b6208be2c24b972cc406af0daa
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-18 22:35:42 +0100

    :bug:

---
+++ php/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function getName(): string
+{
+    $name = ['Juliana', 'Manuel', 'Valerian', 'Gabriel', 'Letizia', 
+    'Guillermo', 'Amelie', 'Myrtille', 'Chiara', 'Clairette', 'Caetano',
+    'Gabriel'][rand(0,11)];
+
+    return $name;
+}
+
+function lireLigne()
+{
+    global $name;
+    echo $name, "\n";
+
+    return $name;
+}
