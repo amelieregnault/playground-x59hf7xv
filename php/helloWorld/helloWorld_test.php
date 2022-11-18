2c3128753f9c33e69d97826a3c9c2c1e17b6da2d
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-18 17:57:42 +0100

    Update helloWorld_test.php

--- php/helloWorld/helloWorld_test.php
+++ php/helloWorld/helloWorld_test.php
@@ -2,7 +2,7 @@
 
 $answer = file_get_contents('./helloWorld.php');
 
-$pattern = "/<[?]php(\s)*echo\s['\"]Hello World !['\"]\s?;$/gm";
+$pattern = "/<[?]php(\s)*echo\s['\"]Hello World !['\"]\s?;$/";
 
 if (preg_match_all($pattern, $answer)) {
     echo("TECHIO> success true \r\n");
