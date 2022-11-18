65871ca393d7e5ac8582f316a9db496fe07c75e6
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-18 22:29:20 +0100

    amelioration sortie

--- php/helloWorld/helloWorld_test.php
+++ php/helloWorld/helloWorld_test.php
@@ -1,21 +1,23 @@
 <?php 
 
-include "./utile.php";
+include "../utile.php";
 
 extract(execUserCode('./helloWorld.php'));
 
+var_dump($output);
+include ('./helloWorld.php');
+
 if ($retval===0 && count($output) === 1 && $output[0]==='Hello World !') {
-    include ('./helloWorld.php');
     echo("TECHIO> success true \r\n");
     echo("TECHIO> message --channel 'Félicitations ! 🎉 Tu viens de créer ton premier programme PHP.'\r\n");
 } else {
     echo("TECHIO> success false  \r\n");
-    echo("TECHIO> message --channel 'Vérifie que ton code est le même ci-dessus.'\r\n");
+    echo("TECHIO> message --channel 'Vérifie que ton code est le même que dans l’exemple ci-dessus.'\r\n");
     if (!str_contains($answer, '<?php')) {
         echo("TECHIO> message --channel 'Vérifie que tu as bien écrit la balise ouvrante PHP.'\r\n");
     }
     if (!str_contains($answer, ';')) {
-        echo("TECHIO> message --channel 'Tu as peut-être oublié le point virgule à la fin de l\\\' instruction.'\r\n");
+        echo("TECHIO> message --channel 'N’aurais-tu pas oublié le point virgule à la fin de l’instruction.'\r\n");
     }
     if (!str_contains($answer, 'echo')) {
         echo("TECHIO> message --channel 'As-tu écrit correctement la commande echo ?'\r\n");
