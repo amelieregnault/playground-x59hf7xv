e4d664decb73a2b7f25492f72137a1d980c564ef
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-18 18:25:19 +0100

    Update helloWorld_test.php

--- php/helloWorld/helloWorld_test.php
+++ php/helloWorld/helloWorld_test.php
@@ -9,9 +9,9 @@ if (preg_match_all($pattern, $answer)) {
     echo("TECHIO> message --channel 'Félicitations ! 🎉 Tu viens de créer ton premier programme PHP.'\r\n");
 } else {
     echo("TECHIO> success false  \r\n");
-    echo("TECHIO> message --channel 'Vérifie que ton code est le même que dans l'exemple ci-dessus.'\r\n");
+    echo("TECHIO> message --channel 'Vérifie que ton code est le même que dans l\'exemple ci-dessus.'\r\n");
     if (!str_contains($answer, '<?php')) {
-        echo("TECHIO> message --channel 'Vérifie que tu as bien écrit la balise ouvrante PHP'\r\n");
+        echo("TECHIO> message --channel 'Vérifie que tu as bien écrit la balise ouvrante PHP.'\r\n");
     }
     if (!str_contains($answer, ';')) {
         echo("TECHIO> message --channel 'N'aurais-tu pas oublié le point virgule à la fin de l'instruction ?'\r\n");
@@ -19,7 +19,7 @@ if (preg_match_all($pattern, $answer)) {
     if (!str_contains($answer, 'echo')) {
         echo("TECHIO> message --channel 'As-tu écrit correctement la commande echo ?'\r\n");
     }
-    if (!str_contains($answer, '"Hello World !"')) {
+    if (!str_contains($answer, '"Hello World !"') && !str_contains($answer, "\'Hello World !\'")) {
         echo("TECHIO> message --channel 'Vérifie que ta chaîne de caractère \"Hello World !\" est correctement écrit.'\r\n");
     }
 }
