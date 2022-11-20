a478f40b10868ad488328b0f8e7e88a56c69c849
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-20 09:31:40 +0100

    Update debogage.php

--- debogageSimple/debogage.php
+++
@@ -1,8 +0,0 @@
-<?php
-$longueurRect1 = lireLigne()
-$ largeurRect1 = lireLigne();
-$longueurRect2 = lireLigne()
-$largeurRect2 = lireLigne();
-$aireTotale = $longueurRect1 + $ longueurRect2 * $largeur rect2 + $largeurRect 1;
-//On affiche le résultat
-echo 'L\'Aire totale est égale à $aireTotale cm²'
