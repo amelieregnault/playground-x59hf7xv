e145de822e320d2a0359e5fa592ba90b29398526
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-20 10:17:33 +0100

    Update helloWorld_test.php

--- helloWorld/helloWorld_test.php
+++ helloWorld/helloWorld_test.php
@@ -16,15 +16,15 @@ if (str_contains($answer, 'exec') || str_contains($answer, 'script')) {
 
 if ($retval===0 && count($output) === 1 && $output[0]==='Hello World !') {
     echo("TECHIO> success true \r\n");
-    echo("TECHIO> message --channel 'Félicitations ! 🎉 ' Tu viens de créer ton premier programme PHP.\r\n");
+    echo("TECHIO> message --channel 'Félicitations ! 🎉' Tu viens de créer ton premier programme PHP.\r\n");
 } else {
     echo("TECHIO> success false  \r\n");
-    echo("TECHIO> message --channel 'Tips'  Vérifie que ton code est le même que dans l’exemple ci-dessus. toto \r\n");
+    echo("TECHIO> message --channel 'Tips'  Vérifie que ton code est le même que dans l’exemple ci-dessus. \r\n");
     if (!str_contains($answer, '<?php')) {
-        echo("TECHIO> message --channel 'Tips'  Vérifie que tu as bien écrit la balise ouvrante PHP.\r\n");
+        echo("TECHIO> message --channel 'Tips' Vérifie que tu as bien écrit la balise ouvrante PHP.\r\n");
     }
     if (!str_contains($answer, ';')) {
-        echo("TECHIO> message --channel 'N’aurais-tu pas oublié le point virgule à la fin de l’instruction.'\r\n");
+        echo("TECHIO> message --channel 'Tips' N'aurais-tu pas oublié le point virgule à la fin de l'instruction.\r\n");
     }
     if (!str_contains($answer, 'echo')) {
         echo("TECHIO> message --channel 'As-tu écrit correctement la commande echo ?'\r\n");
