e53aa251f306ae56f69eea2fd30a501841219420
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-20 09:03:46 +0100

    ajout exercices arithmetiques

--- debogageSimple/debogage.php
+++ debogageSimple/debogage.php
@@ -1,8 +1,8 @@
 <?php
-$rectWidth1 = lireLigne()
-$ rectLength1 = lireLigne();
-$rectWidth2 = lireLigne()
-$rectLength2 = lireLigne();
-$aireTotale = $rectWidth1 + $ rectWidth2 * $rect Length2 + $rectLength 1;
+$longueurRect1 = lireLigne()
+$ largeurRect1 = lireLigne();
+$longueurRect2 = lireLigne()
+$largeurRect2 = lireLigne();
+$aireTotale = $longueurRect1 + $ longueurRect2 * $largeur rect2 + $largeurRect 1;
 //On affiche le résultat
-echo 'L\'Aire totale est égale à $aireTotale cm²' 
+echo 'L\'Aire totale est égale à $aireTotale cm²'
---
+++ markdown/arithmetiques.md
@@ -0,0 +1,1 @@
+# Arithmétique et Logique
