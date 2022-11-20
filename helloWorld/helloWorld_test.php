ad5b8b67d96e7c8d5997150e4c3ae1f917f505b1
Amélie <77639402+amelieregnault@users.noreply.github.com>
2022-11-20 10:01:24 +0100

    :alembic:

--- helloWorld/helloWorld_test.php
+++ helloWorld/helloWorld_test.php
@@ -16,12 +16,12 @@ if (str_contains($answer, 'exec') || str_contains($answer, 'script')) {
 
 if ($retval===0 && count($output) === 1 && $output[0]==='Hello World !') {
     echo("TECHIO> success true \r\n");
-    echo("TECHIO> message --channel 'Félicitations ! 🎉 Tu viens de créer ton premier programme PHP.'\r\n");
+    echo("TECHIO> message --channel 'Félicitations ! 🎉 \n Tu viens de créer ton premier programme PHP.'\r\n");
 } else {
     echo("TECHIO> success false  \r\n");
-    echo("TECHIO> message --channel 'Vérifie que ton code est le même que dans l’exemple ci-dessus.'\r\n");
+    echo(" 'Vérifie que ton code est le même que dans l’exemple ci-dessus.'\r\n");
     if (!str_contains($answer, '<?php')) {
-        echo("TECHIO> message --channel 'Vérifie que tu as bien écrit la balise ouvrante PHP.'\r\n");
+        echo(" 'Vérifie que tu as bien écrit la balise ouvrante PHP.'\r\n");
     }
     if (!str_contains($answer, ';')) {
         echo("TECHIO> message --channel 'N’aurais-tu pas oublié le point virgule à la fin de l’instruction.'\r\n");
